2d76c0f0d6025e0be01da8a6129ca5c5dd075dc6
ShivanG <shivangramola007@gmail.com>
2023-12-18 12:12:51 +0530

    class

--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const Sample = () => {
-  return <div>Sample</div>;
-};
+class Sample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.fullName = "blame";
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>class component</h2>
+        <button>click</button>
+        <SubClass data={this.fullName} />
+      </div>
+    );
+  }
+}
+
+class SubClass extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    console.log(this.props);
+    return <h1>hello subclass {this.props.data} </h1>;
+  }
+}
 
 export default Sample;
