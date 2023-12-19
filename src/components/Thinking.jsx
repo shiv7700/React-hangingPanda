ef6c2198b70a757c17b51f307aa8e4f06106eddc
ShivanG <shivangramola007@gmail.com>
2023-12-19 17:54:34 +0530

    new

--- src/components/Thinking.jsx
+++ src/components/Thinking.jsx
@@ -1,72 +1,93 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import data from "./data.json";
+import "../App.css";
 
-const Thinking = () => {
+const App = () => {
   const [text, setText] = useState("");
-  const [inStockOnly, setInStockOnly] = useState(false);
+  const [stock, setStock] = useState(false);
 
-  const handleSearch = (e) => {
+  const handleText = (e) => {
+    console.log(e.target.value);
     setText(e.target.value);
   };
 
-  const handleStockChange = () => {
-    setInStockOnly(!inStockOnly);
+  const handleCheck = () => {
+    setStock(!stock);
   };
 
   return (
     <>
-      <form>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={text}
-          onChange={handleSearch}
-        />
-        <label>
+      <h2 className="heading">Thinking in react Assignment</h2>
+      <div className="main-container">
+        <div>
           <input
-            type="checkbox"
-            checked={inStockOnly}
-            onChange={handleStockChange}
-          />{" "}
-          Only show products in stock
-        </label>
-      </form>
-      <h4>Fruits</h4>
-      {data
-        .filter((ele) => ele.category === "Fruits")
-        .filter((element) =>
-          element.name.toLowerCase().includes(text.toLowerCase())
-        )
-        .filter((element) => (inStockOnly ? element.stocked : true))
-        .map((element, index) => {
-          return (
-            <div key={index}>
-              <span style={{ color: element.stocked ? "" : "red" }}>
-                {element.name}
-              </span>
-              {element.price}
+            type="text"
+            value={text}
+            onChange={handleText}
+            placeholder="search"
+            className="search"
+          />
+          <br />
+          <div className="check">
+            <div>
+              <h5>Product in stocks</h5>
             </div>
-          );
-        })}
-      <h4>Vegetables</h4>
-      {data
-        .filter((ele) => ele.category === "Vegetables")
-        .filter((element) =>
-          element.name.toLowerCase().includes(text.toLowerCase())
-        )
-        .filter((element) => (inStockOnly ? element.stocked : true))
-        .map((element, index) => {
-          return (
-            <div key={index}>
-              <span style={{ color: element.stocked ? "" : "red" }}>
-                {element.name}
-              </span>
-              {element.price}
+            <div className="checkbox-wrapper-45">
+              <input
+                id="cbx-45"
+                type="checkbox"
+                checked={stock}
+                onChange={handleCheck}
+              />
+              <label className="cbx" htmlFor="cbx-45">
+                <div className="flip">
+                  <div className="front"></div>
+                  <div className="back">
+                    <svg width="16" height="14" viewBox="0 0 16 14">
+                      <path d="M2 8.5L6 12.5L14 1.5"></path>
+                    </svg>
+                  </div>
+                </div>
+              </label>
             </div>
-          );
-        })}
+          </div>
+
+          <h4>Fruits</h4>
+          {data
+            .filter((ele) => ele.category === "Fruits")
+            .filter((e) => e.name.toLowerCase().includes(text.toLowerCase()))
+            .filter((ele) => (stock ? ele.stocked : true))
+            .map((val, i) => {
+              return (
+                <div key={i} className="price">
+                  <p style={{ color: val.stocked ? "" : "red" }}>{val.name}</p>
+                  <p>{val.price}</p>
+                </div>
+              );
+            })}
+          <h4>Vegetables</h4>
+          {data
+            .filter((ele) => ele.category === "Vegetables")
+            .filter((e) => e.name.toLowerCase().includes(text.toLowerCase()))
+            .filter((ele) => (stock ? ele.stocked : true))
+            .map((val, i) => {
+              return (
+                <div key={i} className="price">
+                  <div>
+                    <p style={{ color: val.stocked ? "" : "red" }}>
+                      {val.name}
+                    </p>
+                  </div>
+                  <div>
+                    <p>{val.price}</p>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
   );
 };
 
-export default Thinking;
+export default App;
